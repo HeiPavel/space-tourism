ba90549053e01119aaa2fdcdf00834a010954f8e
HeiPavel <112711905HeiPavel@users.noreply.github.com>
2023-11-13 19:17:17 +0200

    Add mobile menu as sidebar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import logo from '../../media/icons/logo.svg';
+import close from '../../media/icons/icon-close.svg';
 import { MobileMenu } from "../MobileMenu/MobileMenu";
 
 export const Header = () => {
@@ -9,7 +10,10 @@ export const Header = () => {
             <div className="logo-container">
                 <img src={logo} alt="logo" />
             </div>
-            <nav className="desktop">
+            <nav id="general-navigation-menu">
+                <div id="close-button">
+                    <img src={close} alt="close button" />
+                </div>
                 <NavLink to="/"><span>00</span>HOME</NavLink>
                 <NavLink to="/destination"><span>01</span>DESTINATION</NavLink>
                 <NavLink to="/crew"><span>02</span>CREW</NavLink>
--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, {useEffect} from "react";
 import humburger from '../../media/icons/icon-hamburger.svg';
 
 export const MobileMenu = () => {
+
+    useEffect(() => {
+        const menuButton = document.getElementById('humburger');
+        const closeButton = document.getElementById('close-button');
+        const menu = document.getElementById('general-navigation-menu');
+
+        const handleClick = () => {
+            menu.classList.toggle('show');
+            menuButton.classList.toggle('hide');
+        }
+
+        menuButton.addEventListener('click', handleClick);
+        closeButton.addEventListener('click', handleClick);
+
+        return () => {
+            menuButton.removeEventListener('click', handleClick);
+            closeButton.removeEventListener('click', handleClick);
+        }
+    }, []);
+
     return (
         <div className="mobile-menu">
-            <div className="humburger">
+            <div id="humburger">
                 <img src={humburger} alt="button" />
             </div>
-            <div className="sidebar"></div>
         </div>
     );
 }
